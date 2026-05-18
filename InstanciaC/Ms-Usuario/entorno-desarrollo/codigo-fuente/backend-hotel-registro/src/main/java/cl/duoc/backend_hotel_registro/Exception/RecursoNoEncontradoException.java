8a50620a2fac1090eeea5ce3df0c1f106c49fefc
Alejandro Rivera <ale.riverav@duocuc.cl>
2026-05-17 21:35:18 -0400

    Add files via upload

---
+++ InstanciaC/Ms-Usuario/entorno-desarrollo/codigo-fuente/backend-hotel-registro/src/main/java/cl/duoc/backend_hotel_registro/Exception/RecursoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package cl.duoc.backend_hotel_registro.Exception;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
---
+++ InstanciaC/Ms-Usuario/entorno-desarrollo/codigo-fuente/backend-hotel-registro/src/main/java/cl/duoc/backend_hotel_registro/Exception/ServicioNoDisponibleException.java
@@ -0,0 +1,8 @@
+package cl.duoc.backend_hotel_registro.Exception;
+
+public class ServicioNoDisponibleException extends RuntimeException {
+    public ServicioNoDisponibleException(String mensaje) {
+        super(mensaje);
+    }
+
+}
