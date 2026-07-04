2a537d333977ea7f24c743cd167628bcb3a86ce8
Alejandro Rivera <ale.riverav@duocuc.cl>
2026-07-04 00:52:57 -0400

    Delete InstanciaA/Ms-pagosSwaggerTests/entorno-desarrollo2/codigo-fuente2/pagosms directory

--- InstanciaA/Ms-pagosSwaggerTests/entorno-desarrollo2/codigo-fuente2/pagosms/src/main/java/com/pagos/pagosms/excepcion/RecursoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.pagos.pagosms.excepcion;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
--- InstanciaA/Ms-pagosSwaggerTests/entorno-desarrollo2/codigo-fuente2/pagosms/src/main/java/com/pagos/pagosms/repositorio/PagoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.pagos.pagosms.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.pagos.pagosms.modelo.PagoModelo;
-
-public interface PagoRepositorio extends JpaRepository<PagoModelo, Long> {
-}
