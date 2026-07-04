4d797842984329340b1357625f5c56e075f1e6cd
Alejandro Rivera <ale.riverav@duocuc.cl>
2026-07-04 00:59:31 -0400

    Add files via upload

---
+++ InstanciaA/Ms-trabajosSwaggerTests/entorno-desarrollo3/codigo-fuente/trabajosms/src/main/java/com/trabajos/trabajosms/TrabajosmsApplication.java
@@ -0,0 +1,13 @@
+package com.trabajos.trabajosms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajosmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajosmsApplication.class, args);
+	}
+
+}
---
+++ InstanciaA/Ms-trabajosSwaggerTests/entorno-desarrollo3/codigo-fuente/trabajosms/src/main/java/com/trabajos/trabajosms/repositorio/TrabajoRepositorio.java
@@ -0,0 +1,8 @@
+package com.trabajos.trabajosms.repositorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface TrabajoRepositorio extends JpaRepository<com.trabajos.trabajosms.modelo.TrabajoModelo, Long> {
+
+}
