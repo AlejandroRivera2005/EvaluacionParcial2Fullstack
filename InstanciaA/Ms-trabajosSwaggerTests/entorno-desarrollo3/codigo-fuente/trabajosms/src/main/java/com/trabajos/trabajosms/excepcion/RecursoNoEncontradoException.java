f5f530d2cb3e4ae0a8df2b537a4e232e26828a2b
Alejandro Rivera <ale.riverav@duocuc.cl>
2026-07-04 00:53:17 -0400

    Delete InstanciaA/Ms-trabajosSwaggerTests/entorno-desarrollo3/codigo-fuente/trabajosms directory

--- InstanciaA/Ms-trabajosSwaggerTests/entorno-desarrollo3/codigo-fuente/trabajosms/src/main/java/com/trabajos/trabajosms/excepcion/RecursoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.trabajos.trabajosms.excepcion;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
--- InstanciaA/Ms-trabajosSwaggerTests/entorno-desarrollo3/codigo-fuente/trabajosms/src/main/java/com/trabajos/trabajosms/repositorio/TrabajoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.trabajos.trabajosms.repositorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface TrabajoRepositorio extends JpaRepository<com.trabajos.trabajosms.modelo.TrabajoModelo, Long> {
-
-}
